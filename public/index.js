f4dfed1ac9544d010bd7183a4799e27a7f05f438
Kevin-Bateman <kbateman.work@gmail.com>
2020-02-20 10:11:22 -0700

    now hits correct api

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
 async function showMe() {
-    const res = await fetch("https://thirty-min-app-api.herokuapp.com/persons/1")
+    const res = await fetch("https://thirtyminapp-api.herokuapp.com/persons/1")
     const data = await res.json()
 
     document.getElementById("name").innerText = data.name
